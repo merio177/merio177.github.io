edfed59512de1228a32e152ebe6ba0adaab5a044
merio177 <135670642+merio177@users.noreply.github.com>
2023-12-07 14:17:36 +0000

    new website

---
+++ sketch.js
@@ -0,0 +1,87 @@
+let menu = document.querySelector('.menu-icon');
+let sideMenu = document.querySelector(".side-menu")
+var currentAudio;
+
+menu.onclick = () => {
+    menu.classList.toggle('move');
+    sideMenu.classList.toggle("open-menu");
+}
+window.onscroll = () => {
+    menu.classList.remove('move');
+    sideMenu.classList.remove("open-menu");
+}
+
+// Header Background change on scroll
+let header = document.querySelector("header");
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle("header-active", window.scrollY > 0)
+})
+
+// Scroll to top button
+let scroll = document.querySelector(".scroll-top")
+
+window.addEventListener('scroll', () => {
+    scroll.classList.toggle("scroll-active", window.scrollY >= 400)
+})
+
+// Audio 
+function playTrack(trackID, btnID) {
+    var audio = document.getElementById(trackID);
+    var playBtn = document.getElementById(btnID);
+
+    if (audio.paused) {
+        audio.play();
+        playBtn.classList.replace("bx-play-circle", "bx-pause-circle");
+    }
+
+    else {
+        audio.pause();
+        playBtn.classList.replace("bx-pause-circle", "bx-play-circle");
+    }
+}
+
+function resetBtn(btnID) {
+    var button = document.getElementById(btnID);
+    button.classList.replace("bx-pause-circle", "bx-play-circle");
+}
+
+
+// Email JS
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+    
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == "") {
+            emptyerror();
+        }
+        else {
+            sendmail (name.value, email.value, msg.value);
+            success(); 
+        }
+    })
+}
+validate();
+
+function sendmail (name, email, msg) {
+    emailjs.send("service_vj89oed","template_p300205",{
+        from_name: name,
+        from_email: email,
+        message: msg,
+        });
+}
+
+function emptyerror() {
+    let errorMsg = document.getElementById("empty-error");
+    errorMsg.style.visibility = "visible";
+}
+function success() {
+    let successMsg = document.getElementById("contact-success");
+    let contactForm = document.getElementById("contact-form");
+    successMsg.style.visibility = "visible";
+    contactForm.reset();
+}
